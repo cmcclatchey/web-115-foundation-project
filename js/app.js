6ad82e2342ce1549a272c4da1eba0ab5d7f25e31
cmcclatchey <cmcclatchey@e116-m-18.mchenry.edu>
2016-11-22 14:05:17 -0600

    Foundation Project
    Arizona Vacation

---
+++ js/app.js
@@ -0,0 +1,19 @@
+$(document).foundation();
+
+function change01() {
+    
+    document.getElementById("large-image").src = "arizonapictures/food1.jpg";
+    document.getElementById("detail").innerHTML = "Breakfast at Denny's";
+}
+
+function change02() {
+ 
+    document.getElementById("large-image").src = "arizonapictures/food2.jpg";
+    document.getElementById("detail").innerHTML = "Dinner at the Spotted Donkey";
+}
+
+function change03() {
+    
+    document.getElementById("large-image").src = "arizonapictures/food3.jpg";
+    document.getElementById("detail").innerHTML = "Dessert at the Spotted Donkey";
+   }
